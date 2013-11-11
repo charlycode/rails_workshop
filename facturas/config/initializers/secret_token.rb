5a797e93b377048ba4e00e94a8c37de9ceaf9b04
Erick Camacho <ecamacho.mx@gmail.com>
2013-11-11 11:44:36 -0600

    proyecto facturas creado

---
+++ facturas/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Facturas::Application.config.secret_key_base = '778fb870c97e192ee0f85c1dc1c7236ae478fb88a9e7ab552d3b76e77990af04bd3958bd3dcbbd02aee5dbc3d36c482d142429c6e4d765db160c3df5ef1c38d1'
---
+++ presentacion/04-app/app.md
@@ -0,0 +1,21 @@
+Nuestra app
+===
+
+Facturas: sistema de creación y administración de comprobantes fiscales.
+
+
+
+Creación del proyecto
+===
+
+<pre><code data-trim>
+#verifica que estés usando jruby
+ruby -v
+rails new facturas
+cd facturas
+bundle install
+</code></pre>
+
+
+
+Quitamos la página inicial
